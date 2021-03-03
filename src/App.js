3f41429a4f6d295508dfd3b5e6278e84746dbbba
Marina Monsivais <minamonsi@me.com>
2021-03-02 20:26:16 -0600

    Created and added state hook, effect hook, generated an API key, installed axios, and mounted some data on the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=FFZTWmAzFfkoQTl1D6aBB98sh1czoua3OgYF2j5A')
+    .then((res) => {
+      console.log("axios response;", res.data)
+    setData(res.data)
+    })
+    .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>{data.title}</h1>
+      <h2>{data.date}</h2>
+      <img src={data.url}></img>
+      <p>{data.explanation}</p>
+      
     </div>
   );
 }
