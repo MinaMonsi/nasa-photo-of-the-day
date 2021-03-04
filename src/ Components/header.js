eded53a3f23eaeb71f9ad5bb1ad24d7c3e6d711b
Marina Monsivais <minamonsi@me.com>
2021-03-04 15:21:11 -0600

    Deleted code that wasn't working

---
+++ src/ Components/header.js
@@ -0,0 +1,10 @@
+export default function header (props){
+    const { title } = props
+        return (
+            <div className ="picTitle">
+                <h1>Nasa Pic of The Day</h1>
+            </div>
+        )
+    }
+    
+
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
+
 function App() {
   const [data, setData] = useState([]);
   useEffect(() => {
